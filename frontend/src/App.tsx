ccb8e870c41101588b68ca8a5500e9865be3390b
Matias Bobek <ae_mbobek@idcontractors.net>
2023-01-10 18:03:29 -0300

    set up frontend react-app and apollo-client

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> MERNG social media test</h1>
+    </div>
+  );
+}
+
+export default App;
