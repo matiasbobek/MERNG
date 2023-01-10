cd539025f0fa85d1ce417a9ea9aa59f78b3aff80
Matias Bobek <matiasbobek@gmail.com>
2023-01-10 17:10:24 -0300

    Delete config.ts

--- config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  PORT: 5001,
-  MONGODB_URI:
-    "mongodb+srv://matiasbobek:NP15Q3CMV3uUsxgn@cluster0.jg5tbmj.mongodb.net/merng?retryWrites=true&w=majority",
-  SECRET_JWT_KEY: "someverysecretkey",
-};
-
-export default config;
